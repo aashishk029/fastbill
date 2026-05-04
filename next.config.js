5573ce76333f06eb9817c70da4c07a32f98eceab
Aashish <aashishk029@gmail.com>
2026-05-04 07:12:22 +0000

    Clean: Simplify next.config.js - remove all experimental flags

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Disable all i18n routing - we'll handle language selection in the app
-  i18n: undefined,
-  // Explicitly use App Router
-  appDir: true,
 }
 
 module.exports = nextConfig
